810d4e7c0cf4973c5bae6a5a7f8126d187db74fa
maubarrios <mauricio.barrios@gmail.com>
2020-10-02 13:56:03 -0500

    Improving with sintonization of near

--- main.ts
+++ main.ts
@@ -1,13 +1,29 @@
 basic.forever(function () {
     if (-5 <= input.acceleration(Dimension.X) && input.acceleration(Dimension.X) <= 5) {
         basic.showIcon(IconNames.Yes)
-    } else {
-        if (-5 < input.acceleration(Dimension.X)) {
-            basic.showArrow(ArrowNames.West)
-        } else {
-            if (input.acceleration(Dimension.X) < 5) {
-                basic.showArrow(ArrowNames.East)
-            }
-        }
+    } else if (-5 < input.acceleration(Dimension.X)) {
+        basic.clearScreen()
+        basic.showArrow(ArrowNames.West)
+    } else if (-50 < input.acceleration(Dimension.X)) {
+        basic.clearScreen()
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . # # # #
+            . . . # .
+            . . . . .
+            `)
+    } else if (input.acceleration(Dimension.X) < 5) {
+        basic.clearScreen()
+        basic.showArrow(ArrowNames.East)
+    } else if (input.acceleration(Dimension.X) < 50) {
+        basic.clearScreen()
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            # # # # .
+            . # . . .
+            . . . . .
+            `)
     }
 })
